6de1088ebe8acfd971d72c73258f8ceb4b15064d
abdelrahman mohamed <abdolela670@gmail.com>
2025-03-25 02:53:50 +0200

    fix typo in router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,36 +4,37 @@ import FeaturePage from "../pages/featurePage";
 import ContactPage from "../pages/contactPage";
 import data from "../data";
 
-
-const routes = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/feature/:id",
-		element: <FeaturePage />,
-		loader: ({ params }) => {
-			const feature = data.features.find((elem) => elem.id === params.id);
-			const { title, content, pageImage } = feature;
-			const post = {
-				title,
-				content,
-				pageImage,
-			};
-			console.log(post);
-			return post;
+const routes = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
 		},
-	},
-	{
-		path: "/contact",
-		element: <ContactPage />,
-	},
-	{ basename: "/MacBook-landing-page" },
-]);
+		{
+			path: "/feature/:id",
+			element: <FeaturePage />,
+			loader: ({ params }) => {
+				const feature = data.features.find((elem) => elem.id === params.id);
+				const { title, content, pageImage } = feature;
+				const post = {
+					title,
+					content,
+					pageImage,
+				};
+				console.log(post);
+				return post;
+			},
+		},
+		{
+			path: "/contact",
+			element: <ContactPage />,
+		},
+	],
+	{ basename: "/MacBook-landing-page" }
+);
 
-const Router = ()=>{
-   return <RouterProvider router={routes} />
-}
+const Router = () => {
+	return <RouterProvider router={routes} />;
+};
 
 export default Router;
